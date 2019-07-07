f545ed07cfb69e81f517ed9d2e4da8f58cc12f4c
mkmkay <msholomukund@gmail.com>
2019-07-07 15:59:39 +0530

    small changes

--- app/server.py
+++ app/server.py
@@ -21,8 +21,9 @@ app.mount('/static', StaticFiles(directory='app/static'))
 
 
 data = ImageDataBunch.single_from_classes('', classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-learner = create_cnn(data, models.resnet34)
-learner.load('gokul-sentiment-stage-5n')
+learn = create_cnn(data, models.resnet34)
+# learn.load('gokul-sentiment-stage-5n')
+learn.load('stage-1.pth')
 
 
 async def download_file(url, dest):
@@ -48,10 +49,10 @@ async def setup_learner():
             raise
 
 
-loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
-loop.close()
+# loop = asyncio.get_event_loop()
+# tasks = [asyncio.ensure_future(setup_learner())]
+# learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+# loop.close()
 
 
 @app.route('/')
